ae98955c56f3b2ed71211e5998bf45c780d93567
Jimmy Wen <jimmy.z.wen@gmail.com>
2025-02-17 21:31:27 -0500

    chore: brief setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import profile from './img/LinkedIn_Profile_Pic.jpg';
 import './App.css';
+import { Accordion, AppBar, Button, IconButton, Toolbar } from '@mui/material';
+import Home from '@mui/icons-material/Home';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static" color="inherit">
+        <Toolbar variant="regular">
+          <IconButton edge="start" color="inherit" aria-label="Home" sx={{ mr: 2 }}>
+            <Home />
+          </IconButton>
+          <Button>Resume</Button>
+          <Button>Projects</Button>
+        </Toolbar>
+      </AppBar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TODO - Da websiteeeeeee (all over again)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={profile} />
+        <span>Please excuse the appearance of this site</span>
+        <span>while construction is in progress...</span>
+        
       </header>
     </div>
   );
